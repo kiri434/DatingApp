a082a66b5b23c025b34ef4777e45a1af590092b7
Dmitriy Olenich <dolenich@outlook.com>
2018-10-08 16:04:51 +0300

    Test commit

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -10,5 +10,6 @@ namespace DatingApp.API.Data
 
         public DbSet<ValueModel> Values { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
     }
 }
